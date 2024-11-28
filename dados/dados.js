1fd9ff19d085334360c8f3486cc099ba9ebb5e2d
maryunnie <mariannygoncalvesmoreira@gmail.com>
2024-11-28 17:11:18 -0400

    up

--- dados/dados.js
+++ dados/dados.js
@@ -1,118 +1,142 @@
-const historicoInflacao = [
-    { id: 1, ano: 2015, mes: 1, ipca: 1.24 },
-    { id: 2, ano: 2015, mes: 2, ipca: 1.22 },
-    { id: 3, ano: 2015, mes: 3, ipca: 1.32 },
-    { id: 4, ano: 2015, mes: 4, ipca: 0.71 },
-    { id: 5, ano: 2015, mes: 5, ipca: 0.74 },
-    { id: 6, ano: 2015, mes: 6, ipca: 0.79 },
-    { id: 7, ano: 2015, mes: 7, ipca: 0.62 },
-    { id: 8, ano: 2015, mes: 8, ipca: 0.22 },
-    { id: 9, ano: 2015, mes: 9, ipca: 0.54 },
-    { id: 10, ano: 2015, mes: 10, ipca: 0.82 },
-    { id: 11, ano: 2015, mes: 11, ipca: 1.01 },
-    { id: 12, ano: 2015, mes: 12, ipca: 0.96 },
-    { id: 13, ano: 2016, mes: 1, ipca: 1.27 },
-    { id: 14, ano: 2016, mes: 2, ipca: 0.9 },
-    { id: 15, ano: 2016, mes: 3, ipca: 0.43 },
-    { id: 16, ano: 2016, mes: 4, ipca: 0.61 },
-    { id: 17, ano: 2016, mes: 5, ipca: 0.78 },
-    { id: 18, ano: 2016, mes: 6, ipca: 0.35 },
-    { id: 19, ano: 2016, mes: 7, ipca: 0.52 },
-    { id: 20, ano: 2016, mes: 8, ipca: 0.44 },
-    { id: 21, ano: 2016, mes: 9, ipca: 0.08 },
-    { id: 22, ano: 2016, mes: 10, ipca: 0.26 },
-    { id: 23, ano: 2016, mes: 11, ipca: 0.18 },
-    { id: 24, ano: 2016, mes: 12, ipca: 0.3 },
-    { id: 25, ano: 2017, mes: 1, ipca: 0.38 },
-    { id: 26, ano: 2017, mes: 2, ipca: 0.33 },
-    { id: 27, ano: 2017, mes: 3, ipca: 0.25 },
-    { id: 28, ano: 2017, mes: 4, ipca: 0.14 },
-    { id: 29, ano: 2017, mes: 5, ipca: 0.31 },
-    { id: 30, ano: 2017, mes: 6, ipca: -0.23 },
-    { id: 31, ano: 2017, mes: 7, ipca: 0.24 },
-    { id: 32, ano: 2017, mes: 8, ipca: 0.19 },
-    { id: 33, ano: 2017, mes: 9, ipca: 0.16 },
-    { id: 34, ano: 2017, mes: 10, ipca: 0.42 },
-    { id: 35, ano: 2017, mes: 11, ipca: 0.28 },
-    { id: 36, ano: 2017, mes: 12, ipca: 0.44 },
-    { id: 37, ano: 2018, mes: 1, ipca: 0.29 },
-    { id: 38, ano: 2018, mes: 2, ipca: 0.32 },
-    { id: 39, ano: 2018, mes: 3, ipca: 0.09 },
-    { id: 40, ano: 2018, mes: 4, ipca: 0.22 },
-    { id: 41, ano: 2018, mes: 5, ipca: 0.4 },
-    { id: 42, ano: 2018, mes: 6, ipca: 1.26 },
-    { id: 43, ano: 2018, mes: 7, ipca: 0.33 },
-    { id: 44, ano: 2018, mes: 8, ipca: -0.09 },
-    { id: 45, ano: 2018, mes: 9, ipca: 0.48 },
-    { id: 46, ano: 2018, mes: 10, ipca: 0.45 },
-    { id: 47, ano: 2018, mes: 11, ipca: -0.21 },
-    { id: 48, ano: 2018, mes: 12, ipca: 0.15 },
-    { id: 49, ano: 2019, mes: 1, ipca: 0.32 },
-    { id: 50, ano: 2019, mes: 2, ipca: 0.43 },
-    { id: 51, ano: 2019, mes: 3, ipca: 0.75 },
-    { id: 52, ano: 2019, mes: 4, ipca: 0.57 },
-    { id: 53, ano: 2019, mes: 5, ipca: 0.13 },
-    { id: 54, ano: 2019, mes: 6, ipca: 0.01 },
-    { id: 54, ano: 2019, mes: 7, ipca: 0.19 },
-    { id: 55, ano: 2019, mes: 8, ipca: 0.11 },
-    { id: 56, ano: 2019, mes: 9, ipca: -0.04 },
-    { id: 57, ano: 2019, mes: 10, ipca: 0.1 },
-    { id: 58, ano: 2019, mes: 11, ipca: 0.51 },
-    { id: 59, ano: 2019, mes: 12, ipca: 1.15 },
-    { id: 60, ano: 2020, mes: 1, ipca: 0.21 },
-    { id: 61, ano: 2020, mes: 2, ipca: 0.25 },
-    { id: 63, ano: 2020, mes: 3, ipca: 0.07 },
-    { id: 64, ano: 2020, mes: 4, ipca: -0.31 },
-    { id: 65, ano: 2020, mes: 5, ipca: -0.38 },
-    { id: 66, ano: 2020, mes: 6, ipca: 0.26 },
-    { id: 67, ano: 2020, mes: 7, ipca: 0.36 },
-    { id: 68, ano: 2020, mes: 8, ipca: 0.24 },
-    { id: 69, ano: 2020, mes: 9, ipca: 0.64 },
-    { id: 70, ano: 2020, mes: 10, ipca: 0.86 },
-    { id: 71, ano: 2020, mes: 11, ipca: 0.89 },
-    { id: 72, ano: 2020, mes: 12, ipca: 1.35 },
-    { id: 73, ano: 2021, mes: 1, ipca: 0.25 },
-    { id: 74, ano: 2021, mes: 2, ipca: 0.86 },
-    { id: 75, ano: 2021, mes: 3, ipca: 0.93 },
-    { id: 76, ano: 2021, mes: 4, ipca: 0.31 },
-    { id: 77, ano: 2021, mes: 5, ipca: 0.83 },
-    { id: 78, ano: 2021, mes: 6, ipca: 0.53 },
-    { id: 79, ano: 2021, mes: 7, ipca: 0.96 },
-    { id: 80, ano: 2021, mes: 8, ipca: 0.87 },
-    { id: 81, ano: 2021, mes: 9, ipca: 1.16 },
-    { id: 82, ano: 2021, mes: 10, ipca: 1.25 },
-    { id: 83, ano: 2021, mes: 11, ipca: 0.95 },
-    { id: 84, ano: 2021, mes: 12, ipca: 0.73 },
-    { id: 85, ano: 2022, mes: 1, ipca: 0.54 },
-    { id: 86, ano: 2022, mes: 2, ipca: 1.01 },
-    { id: 87, ano: 2022, mes: 3, ipca: 1.62 },
-    { id: 88, ano: 2022, mes: 4, ipca: 1.06 },
-    { id: 89, ano: 2022, mes: 5, ipca: 0.47 },
-    { id: 90, ano: 2022, mes: 6, ipca: 0.67 },
-    { id: 91, ano: 2022, mes: 7, ipca: -0.68 },
-    { id: 92, ano: 2022, mes: 8, ipca: -0.36 },
-    { id: 93, ano: 2022, mes: 9, ipca: -0.29 },
-    { id: 94, ano: 2022, mes: 10, ipca: 0.59 },
-    { id: 95, ano: 2022, mes: 11, ipca: 0.41 },
-    { id: 96, ano: 2022, mes: 12, ipca: 0.62 },
-    { id: 97, ano: 2023, mes: 1, ipca: 0.53 },
-    { id: 98, ano: 2023, mes: 2, ipca: 0.84 },
-    { id: 99, ano: 2023, mes: 3, ipca: 0.71 },
-    { id: 100, ano: 2023, mes: 4, ipca: 0.61 },
-    { id: 101, ano: 2023, mes: 5, ipca: 0.23 },
-    { id: 102, ano: 2023, mes: 6, ipca: 0.08 },
-    { id: 103, ano: 2023, mes: 7, ipca: -0.07 },
-    { id: 104, ano: 2023, mes: 8, ipca: 0.23 },
-    { id: 105, ano: 2023, mes: 9, ipca: 0.26 },
-    { id: 106, ano: 2023, mes: 10, ipca: 0.38 },
-    { id: 107, ano: 2023, mes: 11, ipca: 0.28 },
-    { id: 108, ano: 2023, mes: 12, ipca: 0.46 },
-    { id: 109, ano: 2024, mes: 1, ipca: 0.42 },
-    { id: 110, ano: 2024, mes: 2, ipca: 0.83 },
-    { id: 111, ano: 2024, mes: 3, ipca: 0.16 },
-    { id: 112, ano: 2024, mes: 4, ipca: 0.38 },
-    { id: 113, ano: 2024, mes: 5, ipca: 0.46 },
-    { id: 114, ano: 2024, mes: 6, ipca: 0.21 }
-  ];
-  
-  export default historicoInflacao;
+// Define uma coleção de objetos, onde cada objeto representa uma UF (Unidade Federativa) do Brasil
+// Cada objeto contém um 'id' (número único para a UF), o código da UF ('uf') e o nome completo do estado
+const colecaoUf = [
+  {
+      id: 1,
+      uf: "AC",
+      nome: "Acre"
+  },
+  {
+      id: 2,
+      uf: "AL",
+      nome: "Alagoas"
+  },
+  {
+      id: 3,
+      uf: "AM",
+      nome: "Amazonas"
+  },
+  {
+      id: 4,
+      uf: "AP",
+      nome: "Amapá"
+  },
+  {
+      id: 5,
+      uf: "BA",
+      nome: "Bahia"
+  },
+  {
+      id: 6,
+      uf: "CE",
+      nome: "Ceará"
+  },
+  {
+      id: 7,
+      uf: "DF",
+      nome: "Distrito Federal"
+  },
+  {
+      id: 8,
+      uf: "ES",
+      nome: "Espírito Santo"
+  },
+  {
+      id: 9,
+      uf: "GO",
+      nome: "Goiás"
+  },
+  {
+      id: 10,
+      uf: "MA",
+      nome: "Maranhão"
+  },
+  {
+      id: 11,
+      uf: "MG",
+      nome: "Minas Gerais"
+  },
+  {
+      id: 12,
+      uf: "MS",
+      nome: "Mato Grosso do Sul"
+  },
+  {
+      id: 13,
+      uf: "MT",
+      nome: "Mato Grosso"
+  },
+  {
+      id: 14,
+      uf: "PA",
+      nome: "Pará"
+  },
+  {
+      id: 15,
+      uf: "PB",
+      nome: "Paraíba"
+  },
+  {
+      id: 16,
+      uf: "PE",
+      nome: "Pernambuco"
+  },
+  {
+      id: 17,
+      uf: "PI",
+      nome: "Piauí"
+  },
+  {
+      id: 18,
+      uf: "PR",
+      nome: "Paraná"
+  },
+  {
+      id: 19,
+      uf: "RJ",
+      nome: "Rio de Janeiro"
+  },
+  {
+      id: 20,
+      uf: "RN",
+      nome: "Rio Grande do Norte"
+  },
+  {
+      id: 21,
+      uf: "RO",
+      nome: "Rondônia"
+  },
+  {
+      id: 22,
+      uf: "RR",
+      nome: "Roraima"
+  },
+  {
+      id: 23,
+      uf: "RS",
+      nome: "Rio Grande do Sul"
+  },
+  {
+      id: 24,
+      uf: "SC",
+      nome: "Santa Catarina"
+  },
+  {
+      id: 25,
+      uf: "SE",
+      nome: "Sergipe"
+  },
+  {
+      id: 26,
+      uf: "SP",
+      nome: "São Paulo"
+  },
+  {
+      id: 27,
+      uf: "TO",
+      nome: "Tocantins"
+  }]
+
+  // Exporta a coleção de UFs para que possa ser utilizada em outros arquivos do projeto
+  //exports.colecaoUf = colecaoUf;
+  export default colecaoUf;
